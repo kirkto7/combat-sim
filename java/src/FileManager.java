60d7db252ce31fdda82db3e0daeaad5108d5ddb8
kirkto7 <mca.kirkton@gmail.com>
2023-03-16 11:18:59 -0500

    Added file verification

--- java/src/FileManager.java
+++ java/src/FileManager.java
@@ -2,15 +2,30 @@ import java.io.*;
 
 public class FileManager {
     private static final FileManager instance = new FileManager();
+    private final FileParser parser = FileParser.getInstance();
 
     private FileManager(){}
     public static FileManager getInstance() {
         return instance;
     }
 
-    public String importCharacter(String filename) throws IOException{
+    public String importCharacter(String filename) throws IOException, IllegalArgumentException{
+        int extensionIndex = filename.indexOf(".");
+
+        //Verify filename includes extension
+        if(extensionIndex < 0 ) {
+            throw new IOException("Include extension in filename");
+        }
+
+        //Verify file is a .json
+        String extension = filename.substring(extensionIndex);
+        if(!extension.equals(".json")) {
+            throw new IOException("File is not desired type (.json)");
+        }
+
         BufferedReader in = new BufferedReader(new FileReader(filename));
-        return(in.readLine());
+
+        return in.readLine();
     }
 
     public boolean exportResults() {
--- java/src/Main.java
+++ java/src/Main.java
@@ -1,15 +1,39 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
         FileManager test = FileManager.getInstance();
-        try {
-            test.importCharacter("rook.json");
-        } catch (IOException e) {
-            System.out.println("File not found");
-        }
+        FileParser parse = FileParser.getInstance();
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Filename: ");
+
+        String fileName = "";
+        String data = "";
+
+        boolean isValidFile = true;
+        do {
+            fileName = in.nextLine();
+            if(fileName.equalsIgnoreCase("q")) {
+                return;
+            }
+            try {
+                data = test.importCharacter(fileName);
+                isValidFile = true;
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+                isValidFile = false;
+                System.out.println("Enter new filename, or \"q\" to quit: ");
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                isValidFile = false;
+                System.out.println("Enter new filename, or \"q\" to quit: ");
+            }
+        } while (!isValidFile);
+
+
     }
 
 }
