7a8e0e28f0ab7ebcd462420e90dd41711102fe61
kirkto7 <mca.kirkton@gmail.com>
2023-03-25 20:43:02 -0500

    Attack framework set up

--- java/src/Entities/PlayerCharacter.java
+++ java/src/Entities/PlayerCharacter.java
@@ -1,18 +1,11 @@
 package Entities;
 
+import Doables.Attack;
+
 public class PlayerCharacter extends Entity{
 
-    public PlayerCharacter(int[] stats, int baseHP, int speed, int totalLevels, int[] ABILITY_SCORES, int totalHP, int speed1) {
+    public PlayerCharacter(int[] stats, int baseHP, int speed, int totalLevels, int[] ABILITY_SCORES, int totalHP, int speed1, Attack.DmgType[] resistances, Attack.DmgType[] immunities) {
         super(stats, baseHP, speed, totalLevels, resistances, immunities);
     }
 
-    public void addAction(Action a) {
-        this.actions.add(a);
-    }
-    public void addBonusAction(BonusAction ba) {
-        this.bonusActions.add(ba);
-    }
-    public void addReaction(Reaction r) {
-        this.reactions.add(r);
-    }
 }
